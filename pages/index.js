c0541fc18c8ec78fa2123efbd2e38a05cf3df016
assoares <alexandressoares383@gmail.com>
2021-01-25 19:10:25 -0300

    adding components

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,55 @@
-import styled from 'styled-components'
+import styled from "styled-components";
+import db from "../db.json";
+
+import Widget from "../src/components/Widget";
 
 const Title = styled.h1`
   font-size: 50px;
   color: ${({ theme }) => theme.colors.primary};
-`
+`;
+
+const BackgroundImage = styled.div`
+  background-image: url(${db.bg});
+  flex: 1;
+  background-size: cover;
+  background-position: center;
+  background-repeat: no-repeat;
+`;
+
+export const QuizContainer = styled.div`
+  width: 100%;
+  max-width: 350px;
+  padding-top: 45px;
+  margin: auto 10%;
+  @media screen and (max-width: 500px) {
+    margin: auto;
+    padding: 15px;
+  }
+`;
 
 export default function Home() {
-  return <Title>My page</Title>
+  return (
+    <BackgroundImage>
+      <QuizContainer>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1>Teste Quiz</h1>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+        <Widget>
+          <Widget.Header>
+            <h1>Teste Quiz</h1>
+          </Widget.Header>
+          <Widget.Content>
+            <h1>Teste Quiz</h1>
+            <p>Lorem ipsum</p>
+          </Widget.Content>
+        </Widget>
+      </QuizContainer>
+    </BackgroundImage>
+  );
 }
